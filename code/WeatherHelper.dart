173b6c93f97de619d219fec02d46840af6a6bf5b
AbolfazlAlz <mrabolfazlalz@gmail.com>
2020-08-28 15:20:26 +0430

    Add choose city option

--- code/WeatherHelper.dart
+++ code/WeatherHelper.dart
@@ -6,8 +6,8 @@ class WeatherHelper {
   String _apiAddress = "http://api.openweathermap.org/data/2.5/weather";
   WeatherHelper(String this.apiKey) {}
 
-  dynamic getWeather() async {
-    final address = "$_apiAddress?appid=$apiKey&q=shahrud,%20ir";
+  dynamic getWeather(String city, String state) async {
+    final address = "$_apiAddress?appid=$apiKey&q=$city,$state";
     var request = await HttpClient().getUrl(Uri.parse(address));
     var response = await request.close();
 
--- code/program.dart
+++ code/program.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'WeatherHelper.dart';
 
 main(List<String> args) {
   final weather = WeatherHelper("927029a9c3dcad812caa919dbeff17e2");
-  Future<dynamic> data = weather.getWeather();
+  print("Enter city name: ");
+  final city = stdin.readLineSync();
+  print("Enter the state name: ");
+  final state = stdin.readLineSync();
+  Future<dynamic> data = weather.getWeather(city, state);
   data.then((value) => {
-        print("Weather in Shahroud: " +
+        print("Temperture in $city: " +
             value['main']['temp'].toString() +
             " Kelvin")
       });
